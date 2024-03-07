6dde761638cfc920d8475890155c298fba47896c
HuHe86 <60959258+HuHe86@users.noreply.github.com>
2024-03-07 10:18:02 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,8 @@
-let MAX = 0
 input.onGesture(Gesture.Shake, function () {
     basic.showNumber(randint(1, MAX))
 })
+let MAX = 0
+MAX = 6
 basic.forever(function () {
     if (input.buttonIsPressed(Button.A)) {
         MAX = 6
